ddb7d430ee20e1ec11d87dc834483aea320b7387
Ivan Kusturic <ivan.kusturic@gmail.com>
2021-06-07 14:48:14 +0200

    Created simple Flask application.

---
+++ app.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 17 12:51:31 2021
+
+@author: Admin
+"""
+
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+
+from db import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True #/ this has to be used in order to receive "Request does not contain an access token"
+app.secret_key = 'dragana'
+api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()    
+
+jwt = JWT(app, authenticate, identity) # /auth
+
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(StoreList, '/stores')
+api.add_resource(ItemList, '/items')
+
+api.add_resource(UserRegister, '/register')
+
+if __name__ == '__main__':
+    db.init_app(app)
+    app.run(port=5000)  # important to mention debug=True. 5000 is the default port - can be omitted
